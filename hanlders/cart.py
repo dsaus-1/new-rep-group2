61000833971667a8a9ca3e043a54aa0ae9a063ac
Dima Saushkin <saushkin.reg@gmail.com>
2022-11-28 11:45:39 +0300

    'Added func get_cart and test'

--- hanlders/cart.py
+++ hanlders/cart.py
@@ -1,6 +1,26 @@
+import json
+
 def put_product_to_cart(data):
     pass
 
 
 def get_cart(data):
-    pass
+    '''
+     Функция по выводу корзины
+    '''
+    with open('data/cart.json', encoding='utf-8') as read_file:
+        read_cart = json.load(read_file)
+        count = 0
+        output = []
+        if read_cart == []:
+            return {
+                "code": 404,
+                "message": "В корзине нет товаров"
+                }
+        for i in read_cart:
+            count += 1
+            output.append(f"{count}. {i['name']} ({i['price']} руб/кг добавлено {i['num']} штук")
+        return {
+            "code": 200,
+            "message": '\n'.join(output)
+        }
